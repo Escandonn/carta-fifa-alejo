3d07ac5312d3eefebc1611c19a1cf76b69dcb1b0
Your Name <alejandroescandonescobar0@gmail.com>
2026-06-13 22:14:36 -0500

    rduccion

--- carta-fifa-alejo/src/App.jsx
+++ carta-fifa-alejo/src/App.jsx
@@ -1,13 +1,11 @@
 import MatrixBackground from './components/MatrixBackground';
 import Card from './components/Card';
-import WalkingSprite from './components/WalkingSprite';
 import Chatbot from './components/Chatbot';
 
 function App() {
   return (
     <div className="min-h-screen w-full flex items-center justify-center font-['Arial_Black',sans-serif] overflow-hidden relative selection:bg-[#00ff66] selection:text-black">
       <MatrixBackground />
-      <WalkingSprite />
       <Card />
       <Chatbot />
     </div>
